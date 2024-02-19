78fa192c6e8ce1d9df538d32b90bc5311525e640
kim980onit <kimanikamau971@gmail.com>
2024-02-19 14:24:53 +0300

    sorting_algorithms

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,9 +1,9 @@
 #include "sort.h"
 
 /**
- * swap_ints - swap two integers
- * @a: paremeter of the first interger to swap
- * @b: paremeter of the second integer to swap
+ * swap_ints - Swap two integers in an array.
+ * @a: The first integer to swap.
+ * @b: The second integer to swap.
  */
 void swap_ints(int *a, int *b)
 {
@@ -13,14 +13,15 @@ void swap_ints(int *a, int *b)
 	*a = *b;
 	*b = tmp;
 }
+
 /**
- * bubble_sort - sort an array of integer in ascending order
- * @array: an array of integer to sort
- * @sort: size of the array
+ * bubble_sort - Sort an array of integers in ascending order.
+ * @array: An array of integers to sort.
+ * @size: The size of the array.
  *
- * description: print the array after each swap
+ * Description: Prints the array after each swap.
  */
-void buble_sort(int *array, size_t size)
+void bubble_sort(int *array, size_t size)
 {
 	size_t i, len = size;
 	bool bubbly = false;
@@ -35,7 +36,7 @@ void buble_sort(int *array, size_t size)
 		{
 			if (array[i] > array[i + 1])
 			{
-				swap_ints(array + i, array + i +1);
+				swap_ints(array + i, array + i + 1);
 				print_array(array, size);
 				bubbly = false;
 			}
